431fd2ff36f315bf1fcc1f1a6bc5223fad08dff2
wifimoon <vyache-nov@mail.ru>
2022-07-31 23:26:29 +0300

    added delay to reqest counter

--- grandmaster/authentication/views.py
+++ grandmaster/authentication/views.py
@@ -8,7 +8,8 @@ from .utils import send_sms_code, generate_code
 from .models import PhoneOTP
 
 MAX_SEND_TIMES = 10
-SECONDS_DELAY_BETWEEN_REQUESTS = 50
+SECONDS_DELAY_BETWEEN_REQUESTS_TO_LOCK = 50
+SECONDS_DELAY_BETWEEN_REQUESTS_TO_INCREMENT = 600
 
 
 class ValidatePhoneSendOTP(APIView):
@@ -26,13 +27,14 @@ class ValidatePhoneSendOTP(APIView):
                         'status': False,
                         'details': 'Code send request limit exceeded, contact customer support.'
                     }, status=status.HTTP_423_LOCKED)
-                elif datetime.datetime.now() - last.last_modified.replace(tzinfo=None) < datetime.timedelta(seconds=SECONDS_DELAY_BETWEEN_REQUESTS):
+                elif datetime.datetime.now() - last.last_modified.replace(tzinfo=None) < datetime.timedelta(seconds=SECONDS_DELAY_BETWEEN_REQUESTS_TO_LOCK):
                     return Response({
                         'status': False,
                         'details': 'Wait 50 seconds to reqest new code'
                     }, status=status.HTTP_429_TOO_MANY_REQUESTS)
                 else:
-                    last.count += 1
+                    if datetime.datetime.now() - last.last_modified.replace(tzinfo=None) < SECONDS_DELAY_BETWEEN_REQUESTS_TO_INCREMENT:
+                        last.count += 1
                     last.save()
                     send_sms_code(phone, code)
                     return Response({
