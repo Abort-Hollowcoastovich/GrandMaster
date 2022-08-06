bfda7899e40338ca3abcd9c6f12f578cda6484b7
Abort-Hollowcoastovich <58476378+Abort-Hollowcoastovich@users.noreply.github.com>
2022-08-06 19:36:48 +0300

    biba

--- grandmaster/event/models.py
+++ grandmaster/event/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from .utils import PathAndHash
+from authentication.models import User
 
 
 class Event(models.Model):
@@ -7,11 +9,14 @@ class Event(models.Model):
     address = models.CharField(max_length=200)
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(auto_now_add=True)
-    cover = models.ImageField()
+    cover = models.ImageField(upload_to=PathAndHash('events/covers'), null=True)
     open = models.BooleanField(default=True)
-    number = models.IntegerField()
+    hidden = models.BooleanField(default=False)
+    number = models.IntegerField(default=0)
+    members = models.ManyToManyField(User, related_name='events')
 
     class Meta:
+        db_table = 'events'
         ordering = ['start_date']
 
     @property
--- grandmaster/event/serializers.py
+++ grandmaster/event/serializers.py
@@ -3,6 +3,8 @@ from .models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Event
-        fields = ['name', 'description', 'address', 'start_date', 'end_date', 'cover', 'number']
+        fields = ['pk', 'name', 'description', 'address', 'start_date', 'end_date', 'cover', 'number', 'members']
+        read_only_fields = ['pk', 'number']
