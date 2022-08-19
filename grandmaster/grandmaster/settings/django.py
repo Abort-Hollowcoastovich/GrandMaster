0bf0b7f99df38fd09b3a8907b18773ead1342eaf
wifimoon <vyache-nov@mail.ru>
2022-08-19 16:24:05 +0300

    added static

--- grandmaster/grandmaster/settings/django.py
+++ grandmaster/grandmaster/settings/django.py
@@ -87,8 +87,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Other
