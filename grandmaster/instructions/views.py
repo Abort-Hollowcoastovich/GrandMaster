8d32e69ae866594e381413aa6cb1beeceeae2a89
wifimoon <vyache-nov@mail.ru>
2022-08-20 20:27:37 +0300

    added hiddening

--- grandmaster/instructions/views.py
+++ grandmaster/instructions/views.py
@@ -3,9 +3,17 @@ from rest_framework.permissions import DjangoModelPermissions
 
 from .models import Instruction
 from .serializers import InstructionSerializer
+from authentication.models import User
 
 
 class InstructionViewSet(ModelViewSet):
     queryset = Instruction.objects.all()
     serializer_class = InstructionSerializer
     permission_classes = [DjangoModelPermissions]
+
+    def get_queryset(self):
+        user = self.request.user
+        if User.Group.ADMINISTRATOR in user or User.Group.MODERATOR in user:
+            return Instruction.objects.all()
+        return Instruction.objects.filter(hidden=False)
+
--- grandmaster/videos/urls.py
+++ grandmaster/videos/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import VideoViewSet
 
 router = DefaultRouter()
-router.register('', VideoViewSet)
+router.register('', VideoViewSet, basename='videos')
 
 urlpatterns = [
     path('', include(router.urls)),
