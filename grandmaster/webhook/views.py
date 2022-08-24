c2f04edf6c6d55358540d31211325934002aa338
wifimoon <vyache-nov@mail.ru>
2022-08-24 18:12:22 +0300

    webhook update

--- grandmaster/webhook/views.py
+++ grandmaster/webhook/views.py
@@ -1,12 +1,19 @@
-import json
+from urllib.parse import parse_qs, unquote
 
+from django.core.exceptions import BadRequest
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
 @api_view(['GET', 'POST'])
 def webhook(request):
-    print("request", request)
-    print("body", request.body)
-    print("method", request.method)
-    return Response({'detail': 'hello'})
+    try:
+        s = request.body.decode('utf8')
+        params = parse_qs(unquote(s))
+        event = params['event'][0]
+        _id = params['data[FIELDS][ID]'][0]
+    except Exception:
+        raise BadRequest
+    if event == 'ONCRMCONTACTUPDATE':
+        print('bitrix update', event, id)  # update
+    return Response(status=200)
