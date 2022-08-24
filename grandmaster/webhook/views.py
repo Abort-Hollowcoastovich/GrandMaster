65252fa4ffa7c4d17b11a7937dd160d8101ac810
wifimoon <vyache-nov@mail.ru>
2022-08-24 18:04:39 +0300

    fix

--- grandmaster/webhook/views.py
+++ grandmaster/webhook/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 
 @api_view(['GET', 'POST'])
 def webhook(request):
-    print("request", request.REQUEST)
+    print("request", request)
     print("body", request.body)
     print("method", request.method)
     return Response({'detail': 'hello'})
