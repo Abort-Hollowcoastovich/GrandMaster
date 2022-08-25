5274e825b14bcd86a1198b5706c0052c5bccb203
wifimoon <vyache-nov@mail.ru>
2022-08-25 15:48:32 +0300

    hot fix

--- grandmaster/visit_log/serializers.py
+++ grandmaster/visit_log/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
 
+from authentication.admin import User
 from schedule.serializers import ScheduleSerializer
 from .models import VisitLog
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'full_name'
+        ]
+        read_only_fields = [
+            'id',
+            'full_name'
+        ]
+
+
 class VisitLogSerializer(serializers.ModelSerializer):
     schedule = ScheduleSerializer()
+    attending = UserSerializer(many=True)
 
     class Meta:
         model = VisitLog
