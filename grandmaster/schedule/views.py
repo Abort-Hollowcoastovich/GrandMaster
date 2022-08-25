f8d74738c7e9cf4c5821737e4da24109af7977e5
wifimoon <vyache-nov@mail.ru>
2022-08-25 19:25:41 +0300

    fix

--- grandmaster/schedule/views.py
+++ grandmaster/schedule/views.py
@@ -1,33 +1,12 @@
-import json
-
-from django.core.exceptions import BadRequest
 from rest_framework import status
 from rest_framework.exceptions import NotFound, ValidationError
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
-from django_filters.rest_framework import DjangoFilterBackend
 
 from authentication.models import User
 from .models import Schedule
-from .serializers import ScheduleSerializer
-
-
-# class ScheduleViewSet(ModelViewSet):
-#     permission_classes = [DjangoModelPermissions]
-#     serializer_class = ScheduleSerializer
-#     filter_backends = [DjangoFilterBackend]
-#     filterset_fields = ['gym', 'sport_group']
-#
-#     def get_queryset(self):
-#         user = self.request.user
-#         if User.Group.ADMINISTRATOR in user or User.Group.MODERATOR in user:
-#             return Schedule.objects.all()
-#         elif User.Group.TRAINER in user:
-#             return Schedule.objects.filter(sport_group__trainer=user)
-#         return Schedule.objects.none()
 
 
 def serialize_schedules(sport_group_id: int, gym_id: int, schedules):
