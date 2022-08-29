d8ad9f21a416bb3731be456c297851a95a7cdc34
wifimoon <vyache-nov@mail.ru>
2022-08-29 13:30:13 +0300

    webhook

--- grandmaster/webhook/utils.py
+++ grandmaster/webhook/utils.py
@@ -248,3 +248,53 @@ class UserBuilder:
         except Exception as e:
             logger.error(f"file download error: {str(e)}")
         return None
+
+
+def map_user(user, mock_user):
+    user.b24_id = mock_user.b24_id
+    user.photo = mock_user.photo
+    user.gender = mock_user.gender
+    user.first_name = mock_user.first_name
+    user.last_name = mock_user.last_name
+    user.middle_name = mock_user.middle_name
+    user.birth_date = mock_user.birth_date
+    user.contact_type = mock_user.contact_type
+    user.phone_number = mock_user.phone_number
+    # TODO: change gym and trainer
+    user.sport_school = mock_user.sport_school
+    user.department = mock_user.department
+    user.trainer_name = mock_user.trainer_name
+    user.training_place = mock_user.training_place
+    user.tech_qualification = mock_user.tech_qualification
+    user.sport_qualification = mock_user.sport_qualification
+    user.weight = mock_user.weight
+    user.height = mock_user.height
+
+    user.region = mock_user.region
+    user.city = mock_user.city
+    user.address = mock_user.address
+    user.school = mock_user.school
+    user.med_certificate_date = mock_user.med_certificate_date
+    user.insurance_policy_date = mock_user.insurance_policy_date
+    # TODO: change father
+    user.father_full_name = mock_user.father_full_name
+    user.father_birth_date = mock_user.father_birth_date
+    user.father_phone_number = mock_user.father_phone_number
+    user.father_email = mock_user.father_email
+    # TODO: change mother
+    user.mother_full_name = mock_user.mother_full_name
+    user.mother_birth_date = mock_user.mother_birth_date
+    user.mother_phone_number = mock_user.mother_phone_number
+    user.mother_email = mock_user.mother_email
+
+    user.passport_or_birth_certificate = mock_user.passport_or_birth_certificate
+    user.oms_policy = mock_user.oms_policy
+    user.school_ref = mock_user.school_ref
+    user.insurance_policy = mock_user.insurance_policy
+    user.tech_qual_diplo = mock_user.tech_qual_diplo
+    user.med_certificate = mock_user.med_certificate
+    user.foreign_passport = mock_user.foreign_passport
+    user.inn = mock_user.inn
+    user.diploma = mock_user.diploma
+    user.snils = mock_user.snils
+    user.save()
--- grandmaster/webhook/views.py
+++ grandmaster/webhook/views.py
@@ -6,13 +6,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from authentication.models import User
-from webhook.utils import UserBuilder
+from webhook.utils import UserBuilder, map_user
 
 logger = logging.getLogger(__name__)
 
 
 @api_view(['GET', 'POST'])
-def webhook(request):
+def ebhook(request):
     try:
         s = request.body.decode('utf8')
         params = parse_qs(unquote(s))
@@ -29,56 +29,8 @@ def webhook(request):
             logger.error(f"can't find user for bitrix id at webhook update: error: {e}")
             return Response(status=400, data={f'no such user'})
         return Response(status=200)
+    elif event == 'ONCRMCONTACTCREATE':
+        logger.info(f'user created id:{_id}')
     else:
         logger.info(f'not bitrix update event: event:{event}, id:{_id}')
     return Response(status=200)
-
-
-def map_user(user, mock_user):
-    user.b24_id = mock_user.b24_id
-    user.photo = mock_user.photo
-    user.gender = mock_user.gender
-    user.first_name = mock_user.first_name
-    user.last_name = mock_user.last_name
-    user.middle_name = mock_user.middle_name
-    user.birth_date = mock_user.birth_date
-    user.contact_type = mock_user.contact_type
-    user.phone_number = mock_user.phone_number
-    # TODO: change gym and trainer
-    user.sport_school = mock_user.sport_school
-    user.department = mock_user.department
-    user.trainer_name = mock_user.trainer_name
-    user.training_place = mock_user.training_place
-    user.tech_qualification = mock_user.tech_qualification
-    user.sport_qualification = mock_user.sport_qualification
-    user.weight = mock_user.weight
-    user.height = mock_user.height
-
-    user.region = mock_user.region
-    user.city = mock_user.city
-    user.address = mock_user.address
-    user.school = mock_user.school
-    user.med_certificate_date = mock_user.med_certificate_date
-    user.insurance_policy_date = mock_user.insurance_policy_date
-    # TODO: change father
-    user.father_full_name = mock_user.father_full_name
-    user.father_birth_date = mock_user.father_birth_date
-    user.father_phone_number = mock_user.father_phone_number
-    user.father_email = mock_user.father_email
-    # TODO: change mother
-    user.mother_full_name = mock_user.mother_full_name
-    user.mother_birth_date = mock_user.mother_birth_date
-    user.mother_phone_number = mock_user.mother_phone_number
-    user.mother_email = mock_user.mother_email
-
-    user.passport_or_birth_certificate = mock_user.passport_or_birth_certificate
-    user.oms_policy = mock_user.oms_policy
-    user.school_ref = mock_user.school_ref
-    user.insurance_policy = mock_user.insurance_policy
-    user.tech_qual_diplo = mock_user.tech_qual_diplo
-    user.med_certificate = mock_user.med_certificate
-    user.foreign_passport = mock_user.foreign_passport
-    user.inn = mock_user.inn
-    user.diploma = mock_user.diploma
-    user.snils = mock_user.snils
-    user.save()
