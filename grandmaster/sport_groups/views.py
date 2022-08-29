88c2461b557f15d437f376cadfebecda65b9ef10
wifimoon <vyache-nov@mail.ru>
2022-08-29 13:20:32 +0300

    return remove_member

--- grandmaster/sport_groups/views.py
+++ grandmaster/sport_groups/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.decorators import action
 from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from authentication.models import User
@@ -22,6 +24,26 @@ class SportGroupViewSet(ModelViewSet):
             elif user.Group.STUDENT in user:
                 return user.sport_groups
         return SportGroup.objects.none()
+
+    @action(detail=True, methods=['patch'])
+    def remove_member(self, request, *args, **kwargs):
+        try:
+            user = User.objects.get(pk=request.data['id'])
+        except User.DoesNotExist:
+            return Response({
+                'status': False,
+                'details': 'No such user'
+            }, status=status.HTTP_404_NOT_FOUND)
+        except KeyError:
+            return Response({
+                'status': False,
+                'details': 'Must specify user id in body'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        instance = self.get_object()
+        instance.members.remove(user)
+        instance.save()
+        serializer = SportGroupSerializer(instance=instance)
+        return Response(serializer.data)
 
 
 class SportsmenList(generics.ListAPIView):
