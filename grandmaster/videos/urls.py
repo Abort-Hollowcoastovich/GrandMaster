df49e338a75c091cee01d9b568f9b6d28f2bf755
wifimoon <vyache-nov@mail.ru>
2022-08-30 18:38:09 +0300

    added video images

--- grandmaster/videos/urls.py
+++ grandmaster/videos/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import VideoViewSet
+from .views import VideoViewSet, video_image
 
 router = DefaultRouter()
 router.register('', VideoViewSet, basename='videos')
 
 urlpatterns = [
+    path('image/', video_image),
     path('', include(router.urls)),
 ]
--- grandmaster/videos/views.py
+++ grandmaster/videos/views.py
@@ -1,3 +1,9 @@
+import requests
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+
+from rest_framework.exceptions import NotFound
+from rest_framework.request import Request
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 
@@ -17,3 +23,14 @@ class VideoViewSet(ModelViewSet):
                 return Video.objects.all()
         return Video.objects.filter(hidden=False)
 
+
+@api_view(['GET'])
+def video_image(request: Request):
+    params = request.query_params
+    id = params.get('id', None)
+    if id is None:
+        raise NotFound
+    url = f'https://img.youtube.com/vi/{id}/0.jpg'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return HttpResponse(response.content, content_type='image/jpeg')
