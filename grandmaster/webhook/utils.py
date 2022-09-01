f291dae3c776685b6c05bd7a2d9bed50df77f623
wifimoon <vyache-nov@mail.ru>
2022-09-01 17:43:12 +0300

    fix

--- grandmaster/webhook/utils.py
+++ grandmaster/webhook/utils.py
@@ -306,7 +306,7 @@ def map_user(user, mock_user):
     user.inn = mock_user.inn
     user.diploma = mock_user.diploma
     user.snils = mock_user.snils
-    user.other_documents.clear()
+    user.other_documents.all().clear()
     [Document.objects.create(user=user, image=image) for image in mock_user.other_docs]
     user.save()
 
